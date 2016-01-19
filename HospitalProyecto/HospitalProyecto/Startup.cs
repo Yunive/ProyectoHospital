17054453e900e8e66917201ef9fa9c183ec5b75c
yunive <yunivesaucedarojo@hotmail.com>
2016-01-19 10:19:02 -0700

    ahora si

---
+++ HospitalProyecto/HospitalProyecto/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HospitalProyecto.Startup))]
+namespace HospitalProyecto
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
